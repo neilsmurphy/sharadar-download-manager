d29398f0a4707508dcef9538176d1899fff7a0cb
Neil Murphy <neilsmurphy@outlook.com>
2020-05-17 09:44:57 -0400

    Added db capability.

--- update.py
+++ update.py
@@ -44,20 +44,28 @@ sharadar_tables = {
 }
 
 
+def db_exists(args):
+    # If there is a database designated, check for filename in that datapath.
+    if args.save_to == "db":
+        file_path = path_save("test", args)
+        if not file_path.is_file():
+            raise ValueError("No database file matches the directory and name entered.")
+        else:
+            return None
+
+
 def get_today():
     """Return today's date."""
     return datetime.date.today().strftime("%Y-%m-%d")
 
 
-def connect(save_directory, db):
+def connect(filepath):
     """
     Creating a connection to a database. If doesn't exist, then warning and create the database.
     :param save_directory: Directory where the database will be housed.
     :param db: Database name.
     :return conn: Returns a connection object.
     """
-    path = Path(save_directory)
-    filepath = path / db
 
     # Connect to the database.
     return sqlite3.connect(filepath)
@@ -73,73 +81,8 @@ def get_data(table, kwarg):
     return quandl.get_table("SHARADAR/" + table.upper(), paginate=True, **kwarg)
 
 
-def to_db(table, args):
-    """
-    Evaluates the current state of stored data, identifies where the latest rows in the table are,
-    and then seeks out new data from Sharadar, then saves this downloaded data to the table.
-    :param table: String identifying which table to save.
-    :param args: argsparse object.
-    :return None: Nothing to return the module updates a database.
-    """
-    # Retrieve the new data from Quandl
-    print("Getting data, updating {}".format(table))
+def init_dates(table, args):
 
-    end_date = get_today()
-
-    # Connect to the database.
-    conn = connect(args.save_directory, args.db)
-    c = conn.cursor()
-
-    sql = "SELECT MAX({}) FROM {}".format(sharadar_tables[table][0], table)
-    c.execute(sql)
-    start_date = (pd.to_datetime(c.fetchone()[0]) + pd.Timedelta("1 days")).strftime(
-        "%Y-%m-%d"
-    )
-
-    # Exit if the start date equal today.
-    if start_date == end_date:
-        print(
-            "Update from Quandl for {} cancelled since the start date is the same as the end date.".format(
-                table
-            )
-        )
-        return None
-    else:
-        kwarg = {sharadar_tables[table][0]: {"gte": start_date, "lte": end_date}}
-        df_new_data = get_data(table, kwarg)
-    if df_new_data.shape[0] == 0:
-        print("No new data for {}".format(table))
-        return
-    else:
-        pass
-
-    print(df_new_data.head())
-    print(df_new_data.tail())
-
-    # Join the new data from Quandl with the existing dataframe and sort, reset index.
-    df_new_data.to_sql(table, con=conn, if_exists="append", index=False)
-
-    print(
-        "Successfully updated {} with {} rows from {} to {}".format(
-            table, df_new_data.shape[0], start_date, end_date
-        )
-    )
-
-    return None
-
-
-def to_csv(table, args):
-    """
-    Saves the dataframes to csv format:
-    :param data: Dataframe downloaded from Quandl.
-    :param directory: Directory where csv's will be saved.
-    :param table: Table name downloaded and being saved. e.g. 'SEP'
-    :return None:
-    """
-    path = Path(args.directory)
-    path.mkdir(parents=True, exist_ok=True)
-
-    df_csv = pd.DataFrame()
     date_col = sharadar_tables[table][0]
 
     # Get ending date.
@@ -155,69 +98,149 @@ def to_csv(table, args):
         # Arbitrarily old start date will get data starting at the first row.
         date_start = "2000-01-01"
 
-    # Set file path.
-    file_name = args.save_name + "_" + table + ".csv"
-    file_path = path / file_name
+    assert date_start < date_end
 
-    # Open csv if exist to dataframe and get maximum date.
-    if file_path.is_file():
-        df_csv = pd.read_csv(file_path, parse_dates=[date_col], infer_datetime_format=True).sort_values(date_col)
-        date_start = df_csv[date_col].max().strftime("%Y-%m-%d")
+    return date_col, date_start, date_end
+
+
+def path_directory(args):
+    """
+    Set path and make directory if necessary
+    :param args: Parse args
+    :return path: Pathlib path object
+    """
+    path = Path(args.directory)
+    path.mkdir(parents=True, exist_ok=True)
+
+    return path
+
+
+def path_save(table, args):
+    """
+    Set csv filepath.
+    :param args:
+    :return path object:
+    """
+    path = path_directory(args)
+    if args.save_to == "db":
+        if args.save_name:
+            file_name = args.save_name + ".db"
+        else:
+            file_name = "data.db"
+    elif args.save_to == "csv":
+        if args.save_name:
+            file_name = args.save_name + "_" + table + ".csv"
+        else:
+            file_name = table + ".csv"
+
+    return path / file_name
+
+
+def sync_table(table, args):
+    """
+    Saves the dataframes to csv format:
+    :param data: Dataframe downloaded from Quandl.
+    :param directory: Directory where csv's will be saved.
+    :param table: Table name downloaded and being saved. e.g. 'SEP'
+    :return None:
+    """
+
+    df_save = pd.DataFrame()
+    date_col, date_start, date_end = init_dates(table, args)
+
+    file_path = path_save(table, args)
+
+    if args.save_to == "db":
+        # Connect to the database.
+        conn = connect(file_path)
+        c = conn.cursor()
+    else:
+        pass
+
+    if args.save_to == "csv" and file_path.is_file():
+        # Open csv if exist to dataframe and get maximum date.
+        df_save = pd.read_csv(
+            file_path, parse_dates=[date_col], infer_datetime_format=True
+        ).sort_values(date_col)
+
+        # If there is local data, get the most recent date as start date.
+        if df_save.size != 0:
+            date_start = (df_save[date_col].max() + pd.Timedelta("1 days")).strftime(
+                "%Y-%m-%d"
+            )
+        else:
+            pass
+
+    elif args.save_to == "db" and file_path.is_file():
+
+        # Check if table exist.
+        sql_check_table_exist = "SELECT name FROM sqlite_master WHERE type = 'table' AND name = '{}';".format(
+            table
+        )
+        c.execute(sql_check_table_exist)
+        if c.rowcount != -1:
+            sql = "SELECT MAX({}) FROM {}".format(date_col, table)
+            c.execute(sql)
+            date_start = (
+                pd.to_datetime(c.fetchone()[0]) + pd.Timedelta("1 days")
+            ).strftime("%Y-%m-%d")
+
     else:
         pass
 
     # Check start and end dates.
     if date_end < date_start:
-        raise ValueError("Your start date {} is after your end date {}".format(date_start, date_end))
+        raise ValueError(
+            "Your start date {} is after your end date {}. This could be caused because the start dates in your "
+            "saved data are later than the todate entered.".format(date_start, date_end)
+        )
 
     # Get the data between the dates from Quandl
     kwarg = {date_col: {"gte": date_start, "lte": date_end}}
     df_new = get_data(table, kwarg)
-    df_new = df_new.sort_values(date_col)
-    # df_new[date_col] = df_new[date_col].dt.date.astype('object')
 
-    # Save data.
-    if df_csv.size != 0:
-        df_new = pd.concat([df_csv, df_new])
+    # Join dataframes if CSV.
+    if df_save.size != 0 and args.save_to == "csv":
+        df_total = pd.concat([df_save, df_new])
     else:
-        pass
+        df_total = df_new
 
-    df_new = df_new.sort_values('date')
+    if df_total.size != 0:
+        if table == "SF3B":
+            df_total = df_total.sort_values(date_col, ascending=False)
+        else:
+            df_total = df_total.sort_values(
+                [date_col, "ticker"], ascending=[False, True]
+            )
 
-    # df_new = df_new.sort_values(sharadar_tables[table][0])
-    df_new.to_csv(file_path, index=False)
+        if args.save_to == "csv":
+            df_total.to_csv(file_path, index=False)
+        elif args.save_to == "db":
+            # Join the new data from Quandl with the existing dataframe and sort, reset index.
+            df_new.to_sql(table, con=conn, if_exists="append", index=False)
+        else:
+            pass
 
-    return None
-
-
-def sync_table(table, args):
-    """
-    Manages the downloading, saving, printing of a specific table.
-    :param table: The Sharadar table to sync.
-    :param args: Paramaters input by the user.
-    :return None:
-    """
-    # Database
-    if args.save_to == "db":
-        print("databases {}".format(args.save_to))
-
-    # CSV.
-    elif args.save_to == "csv":
-        to_csv(table, args)
-        print("Table {} saved to csv file.".format(table))
-
-    else:
         print(
-            "{} not saved. If you wish to save please provide argument --save_to and either 'csv' or 'db'.".format(
-                table
+            "Saved table {} from {} to {}.".format(
+                table,
+                df_new[date_col].min().strftime("%Y-%m-%d"),
+                df_new[date_col].max().strftime("%Y-%m-%d"),
             )
         )
+    else:
+        print("No data in table {} for saving.".format(table))
 
-    # Terminal.
     if args.print_rows != 0:
-        print("terminal {}".format(args.print_rows))
+        print(
+            "Table: {}, \n{}".format(
+                table, df_total.head(pd.to_numeric(args.print_rows))
+            )
+        )
     else:
         pass
+
+    return df_new
 
 
 def main(args=None):
@@ -235,6 +258,9 @@ def main(args=None):
     api_key = apikey  ### todo delete this
     quandl.ApiConfig.api_key = api_key
 
+    conn = connect(path_save(None, args))
+    c = conn.cursor()
+
     # Download dataframes from quandl. Set the tables.
 
     if args.tables:
@@ -246,41 +272,28 @@ def main(args=None):
                         t, sharadar_tables.keys()
                     )
                 )
+            elif t == "SF3":
+                print(
+                    "The SF3 table has very large data sets that exceed the download limits of sharadar so SF3 "
+                    "is not downloaded as part of this program. If you wish to download SF3, you must do so "
+                    "direclty from the Quandl site using the web api."
+                )
     else:
         tables = sharadar_tables.keys()
     print("tables {}".format(tables))
 
+    # Check db exists
+    db_exists(args)
+
     # Open tables, get data, and save.
     for t in tables:
         sync_table(t, args)
-    #
-    #
-    # elif args.fromdate:
-    #     if args.todate:
-    #         todate = args.todate
-    #     else:
-    #         todate = get_today()
-    #
-    #     for t in tables:
-    #         # Retrieve data.
-    #         kwarg = {sharadar_tables[t][0]: {"gte": args.fromdate, "lte": todate}}
-    #         data = get_data(t, kwarg)
-    #
-    #         # Save files
-    #         if args.csv:
-    #             save_to_csv(data, args.save_directory, t)
-    #         else:
-    #             pass
-    #
-    #         # Print results.
-    #         pd.options.display.max_rows = int(args.display_rows)
-    #         if args.print_rows != 0:
-    #             print("{} - {}".format(t, sharadar_tables[t][1]))
-    #             print(data.head(int(args.print_rows)))
-    #         else:
-    #             pass
+
+    c.close()
+    conn.close()
 
     return None
+
 
 # todo if not used by end.
 def add_bool_arg(parser, name, default=False, help_text=""):
@@ -304,7 +317,7 @@ def parse_args(pargs=None):
         required=False,
         default="",
         help="Tables to be downloaded. If blank all tables will be downloaded. \n"
-        "Tables are: SF1 SF2 SF3 EVENTS SF3A SF3B SEP TICKERS INDICATORS DAILY SP500 ACTIONS SFP",
+        "Tables are: SF1 SF2 SF3 EVENTS SF3A SF3B SEP TICKERS DAILY SP500 ACTIONS SFP",
     )
 
     parser.add_argument(
